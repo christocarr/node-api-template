c642eece6396f8db8a472fa76a136c99e1b21204
christocarr <christocarr@outlook.com>
2019-01-09 21:26:42 +0000

    Create render to views/results

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ let express = require('express');
 let app = express();
 let request = require('request');
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   res.send('Home page');
 });
 
 app.get('/results', (req, res) => {
-  request('http://www.omdbapi.com/?s=cats&apikey=thewdb', (error, response, body) => {
+  request('http://www.omdbapi.com/?s=night&apikey=thewdb', (error, response, body) => {
     if(!error && response.statusCode == 200) {
-      res.send(body);
+      let data = JSON.parse(body);
+      res.render('results', {data: data});
+      // res.send(results['Search'][0]['Title']);
     } 
   })
 });
