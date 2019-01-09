c63dc96adbc84232537d1a67c117a39ba5b1f38e
christocarr <christocarr@outlook.com>
2019-01-09 20:05:02 +0000

    Add api url to omdb

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const app = express();
-const request = require('request');
+let express = require('express');
+let app = express();
+let request = require('request');
 
 app.get('/', (req, res) => {
-  
+  res.send('Home page');
 });
 
-app.get('/results', (res, req) => {
-
+app.get('/results', (req, res) => {
+  request('http://www.omdbapi.com/?s=cats&apikey=thewdb', (error, response, body) => {
+    if(!error && response.statusCode == 200) {
+      res.send(body);
+    } 
+  })
 });
 
 app.listen(3000, () => {
