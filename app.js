2855df5fb9dfda189d95904f30e135f90174889a
Chris Carr <christocarr@outlook.com>
2019-01-10 20:00:40 +0000

    Initial commit

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ let request = require('request');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Home page');
+  res.render('search');
 });
 
 app.get('/results', (req, res) => {
-  request('http://www.omdbapi.com/?s=night&apikey=thewdb', (error, response, body) => {
+  let query = req.query.search;
+  let url = `http://www.omdbapi.com/?s=${query}&apikey=thewdb`;
+  request(url, (error, response, body) => {
     if(!error && response.statusCode == 200) {
       let data = JSON.parse(body);
       res.render('results', {data: data});
